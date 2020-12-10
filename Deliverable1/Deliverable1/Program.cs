135eff08cbaea2ed1463db1e79965af60dfdbb89
Dustin Owens <dustinowens@dustins-mbp.attlocal.net>
2020-12-09 19:19:33 -0500

    updates after grading

--- Deliverable1/Deliverable1/Program.cs
+++ Deliverable1/Deliverable1/Program.cs
@@ -11,72 +11,30 @@ namespace Deliverable1
 
             string userPassword = Console.ReadLine();
 
-            if (valpassword(userPassword) && checkpassword(userPassword))
+            if (userPassword.Length >= 7 && userPassword.Length <= 12)
             {
-                Console.WriteLine("Password valid and accepted");
-
-
+                if (checkpassword(userPassword))
+                {
+                    Console.WriteLine("Password valid and accepted");
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
             }
             else
             {
                 Console.WriteLine("Error");
             }
         }
-        public static Boolean valpassword(string password)
-        {
 
-            if (password.Length > 7 && password.Length < 12)
-            {
-
-                Console.WriteLine("Length Accepted");
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("Password is too short or too long");
-            }
-            return false;
-
-
-
-        }
         public static Boolean checkpassword(string password)
         {
-            Boolean hasNum = false; Boolean hasCap = false; Boolean hasLow = false; Boolean hasExclamation = false; string c;
-            // for (int i = 0; i < password.Length; i++)
-            // {
-            // c = password[i];
-            if (/*Char.IsNumber(c)*/password.Any(Char.IsNumber) && password.Any(Char.IsUpper) && password.Any(Char.IsLower) && password.Contains("!"))
+
+            if (password.Any(Char.IsUpper) && password.Any(Char.IsLower) && password.Contains("!"))
             {
-                hasNum = true;
-                hasCap = true;
-                hasLow = true;
-                hasExclamation = true;
-
-            }
-            // else if (/*Char.IsUpper(c)*/password.Any(Char.IsUpper))
-            // {
-            //  hasCap = true;
-            // }
-            // else if (/*Char.IsLower(c)*/password.Any(Char.IsLower))
-            // {
-            //  hasLow = true;
-            // }
-            // else if (password.Contains("!"))
-            // {
-            //  hasExclamation = true;
-            // }
-
-
-
-
-            //  }
-            if (hasNum && hasCap && hasLow && hasExclamation)
-            {
-                Console.WriteLine("Special Character Accepted");
                 return true;
             }
-            Console.WriteLine("Not acceptable");
             return false;
 
         }
